b495c8a11db96c69d94f65a09f50f70e53466825
Bryan-Cee <cheruiyotbryan@gmail.com>
2023-02-13 13:35:24 +0300

    fix type error

--- src/types.ts
+++ src/types.ts
@@ -19,6 +19,39 @@ export type TransactionTypeWithRegex = {
   regex: RegExp;
 };
 
+export const transactionTypeWithPattern: TransactionTypeWithRegex[] = [
+  {
+    type: TransactionType.MPesaSentTo,
+    keyPhrase: 'sent to',
+    regex: /(?<reference>[A-Z0-9]+)(?: Confirmed\.)? Ksh(?<amount>[\d,]+.\d{2}) (?<transactionType>sent) to (?<recipient>.+) on (?<date>\d{2}\/\d{2}\/\d{2}) at (?<time>\d{1,2}:\d{2} [AP]M).+M-PESA balance is Ksh(?<balance>[\d,]+.\d{2}).+ [Tt]ransaction cost, Ksh(?<transactionCost>[\d,]+.\d{2})/,
+  },
+  {
+    type: TransactionType.MPesaPaidTo,
+    keyPhrase: 'paid to',
+    regex: /(?<reference>[A-Z0-9]+)(?: Confirmed\.)? Ksh(?<amount>[\d,]+.\d{2}) (?<transactionType>paid) to (?<recipient>.+) on (?<date>\d{2}\/\d{2}\/\d{2}) at (?<time>\d{1,2}:\d{2} [AP]M).+M-PESA balance is Ksh(?<balance>[\d,]+.\d{2}).+ [Tt]ransaction cost, Ksh(?<transactionCost>[\d,]+.\d{2})/,
+  },
+  {
+    type: TransactionType.MPesaWithdraw,
+    keyPhrase: 'Withdraw',
+    regex: /(?<reference>[A-Z0-9]+)(?: Confirmed\.)on (?<date>\d{2}\/\d{2}\/\d{2}) at (?<time>\d{1,2}:\d{2} [AP]M)(?<transactionType>Withdraw) Ksh(?<amount>[\d,]+.\d{2}) from (?<agent>.+) New M-PESA balance is Ksh(?<balance>[\d,]+.\d{2}).+Transaction cost(?:, )?Ksh(?:\s|\S)(?<transactionCost>[\d,]+.\d{2})/,
+  },
+  {
+    type: TransactionType.MShwariDeposit,
+    keyPhrase: 'transferred to M-Shwari',
+    regex: /(?<reference>[A-Z0-9]+)(?: Confirmed\.)Ksh(?<amount>[\d,]+.\d{2}) (?<transactionType>transferred) to (?<account>.+) on (?<date>\d{2}\/\d{2}\/\d{2}) at (?<time>\d{1,2}:\d{2} [AP]M).+M-PESA balance is Ksh(?<balance>[\d,]+.\d{2}).+ M-Shwari saving account balance is Ksh(?<mShwariBalance>[\d,]+.\d{2}).+Transaction cost Ksh(?:\s|\S)(?<transactionCost>[\d,]+.\d{2})/,
+  },
+  {
+    type: TransactionType.MShwariWithdraw,
+    keyPhrase: 'transferred from M-Shwari',
+    regex: /(?<reference>[A-Z0-9]+)(?: Confirmed\.)Ksh(?<amount>[\d,]+.\d{2}) (?<transactionType>transferred) from (?<account>.+) on (?<date>\d{2}\/\d{2}\/\d{2}) at (?<time>\d{1,2}:\d{2} [AP]M). M-Shwari balance is Ksh(?<mShwariBalance>[\d,]+.\d{2}).+M-PESA balance is Ksh(?<balance>[\d,]+.\d{2}).+Transaction cost Ksh(?:\s|\S)(?<transactionCost>[\d,]+.\d{2})/,
+  },
+  {
+    type: TransactionType.MPesaDeposit,
+    keyPhrase: 'You have received',
+    regex: /(?<reference>[A-Z0-9]+)(?: Confirmed\.)(?:.+) (?<transactionType>received)? Ksh(?<amount>[\d,]+.\d{2}) from (?<sender>.+) on (?<date>\d{2}\/\d{2}\/\d{2}) at (?<time>\d{1,2}:\d{2} [AP]M).+M-PESA balance is Ksh(?<balance>[\d,]+.\d{2}).+/,
+  },
+];
+
 export type MShwariTransferredToMPesa = {
   reference: string;
   amount: string;
